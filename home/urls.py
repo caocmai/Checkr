aad703ed3ea0b2592e74acdd9dd90e9ccbfdd3c6
caocmai <caomai@rocketmail.com>
2021-10-24 20:13:41 -0700

    Can fetch and save Nasa daily image

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from home.views import HomeView, register
+from home.views import HomeView, downloadImage, register
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home-page'),
-    path('signup/', register, name='sign-up-page')
+    path('signup/', register, name='sign-up-page'),
+    path('getpic/', downloadImage, name='get-pic')
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- home/views.py
+++ home/views.py
@@ -3,6 +3,20 @@ from django.views.generic.list import ListView
 from home.forms import UserRegistrationForm
 from django.contrib import messages
 
+from home.models import NasaImage
+from django.core.files import File 
+from io import BytesIO
+import requests
+
+from django.http import HttpResponse
+
+import uuid   
+
+
+
+import urllib.request
+
+
 # Create your views here.
 
 class HomeView(ListView):
@@ -10,7 +24,9 @@ class HomeView(ListView):
 
     def get(self, request):
         """ GET a list of Pages. """
-        return render(request, 'home/home.html')
+        nasaPics = NasaImage.objects.all()
+        
+        return render(request, 'home/home.html', {'nasaImage': nasaPics[len(nasaPics)-1]})
 
 
 def register(request):
@@ -25,3 +41,18 @@ def register(request):
     else:
         form = UserRegistrationForm()
     return render(request, 'home/register.html', {'form': form})
+
+def downloadImage(request):
+    url = "https://api.nasa.gov/planetary/apod?api_key=uQ7H6lYnPUYvkesIXi8CDO1XhZEEAJO8Wr0Q6oHU"
+    resp = requests.get(url)
+    print(resp.json()['url'])
+    # if resp.status_code != requests.codes.ok:
+    #     #  Error handling here
+    #     print("error")
+    httpUrl = resp.json()['url']
+    result = urllib.request.urlretrieve(httpUrl)
+    nasaPic = NasaImage()            
+    nasaPic.image.save(str(uuid.uuid4())+".jpg", File(open(result[0], 'rb')))
+    nasaPic.save()
+    nasaPics = NasaImage.objects.all()
+    return render(request, 'home/home.html', {'nasaImage': nasaPics[len(nasaPics)-1]})
